f379d6fb4cac1ee309c283439a1ae0036c941d3c
Sandra Kulyniy <sandrakulyniy@gmail.com>
2026-01-19 00:21:18 -0500

    updated

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let username;
 
 document.getElementById("mySubmit").onclick = function() {
     username = document.getElementById("myText").value;
-    document.getElementById("myH1").textContent = 'Hello ${username}';
+    document.getElementById("myH1").textContent = `Hello ${username}`;
 }
